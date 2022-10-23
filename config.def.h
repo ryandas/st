a0de7838b84a520b147ddbd0286727088b74d5a4
ryandas <ryandas@protonmail.com>
2022-10-23 16:22:14 +0530

    Using Fira Code Nerd Font Regular

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code:style=Regular:pixelsize=18:antialias=true:autohint=true";
+static char *font = "Fira Code Nerd Font:style=Regular:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
